b5959b3795a138415b062d3797c57633771f8666
IngoHHacks <69536813+IngoHHacks@users.noreply.github.com>
2020-10-08 11:09:13 +0200

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,51 @@
+package proj1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Logger {
+
+    /**
+     * Writes a string to the log file
+     * @param s String to write to log
+     */
+    public static void log(String s) {
+        try (FileWriter fileWriter = new FileWriter(new File(System.getProperty("user.dir") + "/log.txt"), true)) {
+            fileWriter.write("[" + LocalDateTime.now().toLocalTime().truncatedTo(ChronoUnit.SECONDS) + "]\t" + s + "\n");
+            fileWriter.close();
+        } catch (Exception e) {
+            System.err.println("Failed to write logs.");
+        }
+    }   
+
+    /**
+     * 
+     * @param algorithm Name of the algorithm used
+     * @param chromaticNumber The chromatic number computed by the algorithm
+     * @param time The time (in ms) it took to run the algorithm
+     */
+    public static void logResults(String algorithm, String inputfile, int chromaticNumber, Double time) {
+        try (FileWriter fileWriter = new FileWriter(new File(System.getProperty("user.dir") + "/log.txt"), true)) {
+            fileWriter.write("[" + LocalDateTime.now().toLocalTime().truncatedTo(ChronoUnit.SECONDS) + "]\t" + algorithm + "\t" + inputfile + "\t" + chromaticNumber + "\t" + time + "\n");
+            fileWriter.close();
+        } catch (Exception e) {
+            System.err.println("Failed to write logs.");
+        }
+    }   
+
+    /**
+     * Clears the log file
+     */
+    public static void clearLog() {
+        try (FileWriter fileWriter = new FileWriter(new File(System.getProperty("user.dir") + "/log.txt"))) {
+            fileWriter.write("");
+            fileWriter.close();
+        } catch (Exception e) {
+            System.err.println("Failed to write logs.");
+        }
+    }   
+
+
+}
